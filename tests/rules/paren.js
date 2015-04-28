d8a51c620b8d7b1a4ec10c1da504ddb1f772b3ba
Jxck <block.rxckin.beats@gmail.com>
2015-04-28 17:39:57 +0900

    add tests

---
+++ tests/rules/paren.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var linter = require('eslint').linter;
+var ESLintTester = require('eslint-tester');
+var eslintTester = new ESLintTester(linter);
+
+
+var valid = [
+  '() => {}',
+  '(a) => {}',
+  '(a) => a',
+  '(a) => {\n}',
+  'a.then((foo) => {});',
+  'a.then((foo) => { if (true) {}; });',
+].map(function(code) {
+  return {
+    code: code,
+    ecmaFeatures: { arrowFunctions: true }
+  };
+});
+
+var message = 'arrow function argument should wrap with parens';
+
+
+var invalid = [
+  'a => {}',
+  'a => a',
+  'a => {\n}',
+  'a.then(foo => {});',
+  'a.then(foo => a);',
+  'a(foo => { if (true) {}; });',
+].map(function(code) {
+  return {
+    code: code,
+    ecmaFeatures: { arrowFunctions: true },
+    errors: [{ message: message }]
+  };
+});
+
+eslintTester.addRuleTest('./lib/rules/paren', {
+  valid: valid,
+  invalid: invalid
+});
---
+++ tests/rules/space.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var linter = require('eslint').linter;
+var ESLintTester = require('eslint-tester');
+var eslintTester = new ESLintTester(linter);
+
+
+var valid = [
+  '() => {}',
+  '(a) => {}',
+  'a => a',
+  '() => {\n}',
+].map(function(code) {
+  return {
+    code: code,
+    ecmaFeatures: { arrowFunctions: true }
+  };
+});
+
+var message = '`=>` in arrow fuction should wrap with space';
+
+var invalid = [
+  '()=> {}',
+  '() =>{}',
+  '(a)=> {}',
+  '(a) =>{}',
+  'a =>a',
+  'a=> a',
+  '()=> {\n}',
+  '() =>{\n}',
+].map(function(code) {
+  return {
+    code: code,
+    ecmaFeatures: { arrowFunctions: true },
+    errors: [{ message: message }]
+  };
+});
+
+eslintTester.addRuleTest('./lib/rules/space', {
+  valid: valid,
+  invalid: invalid
+});
