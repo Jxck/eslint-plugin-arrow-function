1cbe9353f52ce3f917ccea40c2211345502e33a7
Jxck <block.rxckin.beats@gmail.com>
2015-04-29 08:42:38 +0900

    export no-condition rule

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   rules: {
     "space": require("./lib/rules/space"),
     "paren": require("./lib/rules/paren"),
+    "no-condition": require("./lib/rules/no-condition"),
   }
 };
