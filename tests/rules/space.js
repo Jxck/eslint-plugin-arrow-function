c56c7a9f16575729e2db95736862ecc74e5e70b3
Jxck <block.rxckin.beats@gmail.com>
2015-06-10 18:09:44 +0900

    adding test

--- tests/rules/space.js
+++ tests/rules/space.js
@@ -1,39 +1,86 @@
 'use strict';
+/*eslint no-multi-spaces:0, space-in-brackets:0 */
 
 var linter = require('eslint').linter;
 var ESLintTester = require('eslint-tester');
 var eslintTester = new ESLintTester(linter);
 
+var TT = { before: true, after: true };
+var TF = { before: true, after: false };
+var FT = { before: false, after: true };
+var FF = { before: false, after: false };
+
+var MB = { message: 'Missing space before =>' };
+var MA = { message: 'Missing space after =>' };
+var UB = { message: 'Unexpected space before =>' };
+var UA = { message: 'Unexpected space after =>' };
 
 var valid = [
-  '() => {}',
-  '(a) => {}',
-  'a => a',
-  '() => {\n}'
-].map(function(code) {
+  [ TT, 'a => a'    ],
+  [ TT, '() => {}'  ],
+  [ TT, '(a) => {}' ],
+
+  [ FT, 'a=> a'     ],
+  [ FT, '()=> {}'   ],
+  [ FT, '(a)=> {}'  ],
+
+  [ TF, 'a =>a'     ],
+  [ TF, '() =>{}'   ],
+  [ TF, '(a) =>{}'  ],
+
+  [ FF, 'a=>a'      ],
+  [ FF, '()=>{}'    ],
+  [ FF, '(a)=>{}'   ],
+
+  [ {}, 'a => a'    ],
+  [ {}, '() => {}'  ],
+  [ {}, '(a) => {}' ]
+].map(function(param) {
   return {
-    code: code,
-    args: [ 1, { before: true, after: true }],
+    args: [1, param[0]],
+    code: param[1],
     ecmaFeatures: { arrowFunctions: true }
   };
 });
 
-var message = '`=>` in arrow fuction should wrap with space';
-
 var invalid = [
-  '()=> {}',
-  '() =>{}',
-  '(a)=> {}',
-  '(a) =>{}',
-  'a =>a',
-  'a=> a',
-  '()=> {\n}',
-  '() =>{\n}'
-].map(function(code) {
+  [ TT, 'a=>a',        [MB, MA] ],
+  [ TT, '()=>{}',      [MB, MA] ],
+  [ TT, '(a)=>{}',     [MB, MA] ],
+
+  [ TT, 'a  =>  a',    [UB, UA] ],
+  [ TT, '()  =>  {}',  [UB, UA] ],
+  [ TT, '(a)  =>  {}', [UB, UA] ],
+
+  [ TF, 'a=> a',       [MB, UA] ],
+  [ TF, '()=> {}',     [MB, UA] ],
+  [ TF, '(a)=> {}',    [MB, UA] ],
+
+  [ TF, 'a=>  a',      [MB, UA] ],
+  [ TF, '()=>  {}',    [MB, UA] ],
+  [ TF, '(a)=>  {}',   [MB, UA] ],
+
+  [ FT, 'a =>a',       [UB, MA] ],
+  [ FT, '() =>{}',     [UB, MA] ],
+  [ FT, '(a) =>{}',    [UB, MA] ],
+
+  [ FT, 'a  =>a',      [UB, MA] ],
+  [ FT, '()  =>{}',    [UB, MA] ],
+  [ FT, '(a)  =>{}',   [UB, MA] ],
+
+  [ FF, 'a => a',      [UB, UA] ],
+  [ FF, '() => {}',    [UB, UA] ],
+  [ FF, '(a) => {}',   [UB, UA] ],
+
+  [ FF, 'a  =>  a',    [UB, UA] ],
+  [ FF, '()  =>  {}',  [UB, UA] ],
+  [ FF, '(a)  =>  {}', [UB, UA] ]
+].map(function(param) {
   return {
-    code: code,
     ecmaFeatures: { arrowFunctions: true },
-    errors: [{ message: message }]
+    args: [1, param[0]],
+    code: param[1],
+    errors: param[2]
   };
 });
 
